e7dd2c09c2c3ab984b8c21fe2f6c607b1ffa6203
Tolek1803 <140890377+Tolek1803@users.noreply.github.com>
2024-06-19 21:47:29 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,28 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const { createServer } = require('http');
 
 const proxyConfig = {
-    target: 'https://www.crazygames.com/game/bloxdhop-io',
+    target: 'https://www.crazygames.com',
     changeOrigin: true,
-    selfHandleResponse: false, // Allow the proxy middleware to handle responses
-    logLevel: 'debug',
+    selfHandleResponse: true,
     pathRewrite: {
-        '^/proxy': '', // Remove /proxy from the beginning of the URL path
+        '^/': '', // Rewrite path to remove leading '/'
     },
 };
 
 const proxy = createProxyMiddleware(proxyConfig);
 
-// Serverless function handler
-const requestHandler = (req, res) => {
-    proxy(req, res, (err) => {
-        if (err) {
-            res.statusCode = 500;
-            res.end(`Proxy error: ${err.message}`);
-        }
-    });
-};
-
 module.exports = (req, res) => {
-    createServer(requestHandler).emit('request', req, res);
+    return proxy(req, res);
 };
